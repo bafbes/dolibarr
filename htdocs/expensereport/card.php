29994bfb1195952a18d682bd72cdde70ba4378af
Bahfir Abbes <bafbes@gmail.com>
2016-01-30 16:45:54 +0100

    Update card.php
    set default tva to EXPENSEREPORT_DEFAULT_VAT if defined, and to MAIN_VAT_DEFAULT_IF_AUTODETECT_FAILS otherwise

--- htdocs/expensereport/card.php
+++ htdocs/expensereport/card.php
@@ -1826,7 +1826,7 @@ else
 
 						// Select VAT
 						print '<td style="text-align:right;">';
-						$defaultvat=-1;
+						$defaultvat=$conf->global->EXPENSEREPORT_DEFAULT_VAT?$conf->global->EXPENSEREPORT_DEFAULT_VAT:$conf->global->MAIN_VAT_DEFAULT_IF_AUTODETECT_FAILS;
 						if (! empty($conf->global->EXPENSEREPORT_NO_DEFAULT_VAT)) $conf->global->MAIN_VAT_DEFAULT_IF_AUTODETECT_FAILS = 'none';
 						print '<select class="flat" name="vatrate">';
 						print $form->load_tva('vatrate', ($vatrate!=''?$vatrate:$defaultvat), $mysoc, '', 0, 0, '', true);
