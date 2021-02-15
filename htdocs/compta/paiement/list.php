417466adbd7269bb23ebde7a0a5e6713bd1e4734
abb <bafbes@gmail.com>
2021-02-15 17:56:16 +0100

    fix:Handle constant MAIN_SHOW_SOCIETE2EXTERN in invoice payments list

--- htdocs/compta/paiement/list.php
+++ htdocs/compta/paiement/list.php
@@ -51,7 +51,7 @@ $month	= GETPOST('month', 'int');
 $year = GETPOST('year', 'int');
 
 // Security check
-if ($user->socid) $socid = $user->socid;
+if ($user->socid && empty($conf->global->MAIN_SHOW_SOCIETE2EXTERN)) $socid = $user->socid;
 $result = restrictedArea($user, 'facture', $facid, '');
 
 $paymentstatic = new Paiement($db);
