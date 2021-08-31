4fcc87a205f7e7894a54ac3e7d155c2d71ae2c40
abb <bafbes@gmail.com>
2021-08-31 19:54:21 +0100

    Ecran d'accueil melissa 3:Fix hauteur des éléments de structure+suppression de structures2.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -73,7 +73,7 @@ if ($resql) {
         $(document).ready(function(){
             $("#UR").click(function(){
                 hide_all();
-                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures1.php?id=$obj->fk_object&type=UR",2)?>", success: function(result){
+                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures.php?id=$obj->fk_object&type=UR",2)?>", success: function(result){
                         pos=result.indexOf(' ');
                         n=result.substring(0,pos);
                         result=result.substring(pos+1,50000);
@@ -84,7 +84,7 @@ if ($resql) {
         $(document).ready(function(){
             $("#UD").click(function(){
                 hide_all();
-                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures1.php?id=$obj->fk_object&type=UD",2)?>", success: function(result){
+                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures.php?id=$obj->fk_object&type=UD",2)?>", success: function(result){
                         pos=result.indexOf(' ');
                         n=result.substring(0,pos);
                         result=result.substring(pos+1,50000);
@@ -95,7 +95,7 @@ if ($resql) {
         $(document).ready(function(){
             $("#UL").click(function(){
                 hide_all();
-                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures1.php?id=$obj->fk_object&type=UL",2)?>", success: function(result){
+                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures.php?id=$obj->fk_object&type=UL",2)?>", success: function(result){
                         pos=result.indexOf(' ');
                         n=result.substring(0,pos);
                         result=result.substring(pos+1,50000);
@@ -106,7 +106,7 @@ if ($resql) {
         $(document).ready(function(){
             $("#ASS").click(function(){
                 hide_all();
-                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures1.php?id=$obj->fk_object&type=ASS",2)?>", success: function(result){
+                $.ajax({url: "<?= dol_buildpath("/cgl/tabs/structures.php?id=$obj->fk_object&type=ASS",2)?>", success: function(result){
                         pos=result.indexOf(' ');
                         n=result.substring(0,pos);
                         result=result.substring(pos+1,50000);
