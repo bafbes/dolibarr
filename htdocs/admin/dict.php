05e04626601ea26d4338c715b0c5c05f44a06459
bahfir abbes <bafbes@gmail.com>
2022-09-11 13:21:18 +0200

    fix:admit undescores in currency codes

--- htdocs/admin/dict.php
+++ htdocs/admin/dict.php
@@ -830,7 +830,7 @@ if (GETPOST('actionadd') || GETPOST('actionmodify')) {
 		$_POST["accountancy_code_buy"] = ''; // If empty, we force to null
 	}
 	if ($id == 10 && GETPOSTISSET("code")) {  // Spaces are not allowed into code for tax dictionary
-		$_POST["code"] = preg_replace('/[^a-zA-Z0-9\-\+]/', '', $_POST["code"]);
+		$_POST["code"] = preg_replace('/[^a-zA-Z0-9_\-\+]/', '', $_POST["code"]);
 	}
 
 	// If check ok and action add, add the line
